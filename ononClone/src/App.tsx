92463c37e571867e834f17df546f140560d59764
Özay Melih Yıldız <102221334+Ozaui@users.noreply.github.com>
2025-10-13 21:05:40 +0300

    header design

---
+++ ononClone/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p className="mt-25">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima aut sint
+        voluptas recusandae suscipit, dignissimos repellat dicta libero soluta
+        ex ipsum necessitatibus rem laudantium. Beatae exercitationem nobis
+        assumenda quos maxime repellat ex, consectetur fugit quod. Aliquid
+        quisquam quibusdam iure exercitationem illum porro ratione expedita hic?
+        Deserunt soluta sunt fuga quia.
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ ononClone/src/components/Header.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+
+const Header: React.FC = () => {
+  const [hamburgerButton, setHamburgerButton] = useState(false);
+
+  return (
+    <div className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
+      {/* Header kapsayıcı */}
+      <div className="h-25 flex items-center justify-between px-4">
+        <img className="w-20" src={logo} alt="onon-logo" />
+        <div className="hidden lg:flex items-center">
+          <div className="ml-20 flex gap-4 font-bold">
+            <a href="">ANA SAYFA</a>
+            <a href="">MENÜ</a>
+            <a href="">ŞUBELER</a>
+            <a href="">HABERLER</a>
+            <a href="">HAKKIMIZDA</a>
+            <a href="">İLETİŞİM</a>
+          </div>
+        </div>
+        <div className="flex">
+          <button
+            onClick={() => setHamburgerButton((prev) => !prev)}
+            className="block lg:hidden mr-4 font-bold text-2xl cursor-pointer"
+          >
+            ☰
+          </button>
+          <button className="hidden sm:block mr-4 px-6 py-2.5 border rounded border-[#c7a17a] hover:border-black bg-[#c7a17a] text-white text-sm lg:text-lg font-bold hover:bg-black duration-300">
+            İLETİŞİM
+          </button>
+        </div>
+      </div>
+
+      {/* Hamburger menü */}
+      <div
+        className={`absolute top-full left-0 w-full overflow-hidden transition-[max-height,opacity] duration-300 ease-in-out flex flex-col bg-white shadow-md font-bold
+        ${hamburgerButton ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"}`}
+      >
+        <a href="" className="py-2 px-4">
+          ANA SAYFA
+        </a>
+        <hr className="border-gray-200" />
+        <a href="" className="py-2 px-4">
+          MENÜ
+        </a>
+        <hr className="border-gray-200" />
+        <a href="" className="py-2 px-4">
+          ŞUBELER
+        </a>
+        <hr className="border-gray-200" />
+        <a href="" className="py-2 px-4">
+          HABERLER
+        </a>
+        <hr className="border-gray-200" />
+        <a href="" className="py-2 px-4">
+          HAKKIMIZDA
+        </a>
+        <hr className="border-gray-200" />
+        <a href="" className="py-2 px-4">
+          İLETİŞİM
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
